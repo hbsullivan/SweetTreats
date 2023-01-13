76dbc2cc24165a8d8e0882dd009e8f16019d7c48
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-13 08:59:57 -0800

    add C functionality to flavors class

--- SweetTreats/Controllers/FlavorsController.cs
+++ SweetTreats/Controllers/FlavorsController.cs
@@ -19,5 +19,25 @@ namespace SweetTreats.Controllers
     {
       return View(_db.Flavors.ToList());
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View();
+      }
+      else
+      {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+      }
+    }
   }
 }
