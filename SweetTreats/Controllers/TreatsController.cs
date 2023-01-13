2b4dec49d0fd227f8d291ed4e7fee6ff5144b5bb
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-13 10:32:03 -0800

    add CR funcionality to treats class

---
+++ SweetTreats/Controllers/TreatsController.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using SweetTreats.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SweetTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly SweetTreatsContext _db;
+    public TreatsController(SweetTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View();
+      }
+      else
+      {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats
+                            .Include(treat => treat.JoinEntities)
+                            .ThenInclude(join => join.Flavor)
+                            .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+  }
+}
